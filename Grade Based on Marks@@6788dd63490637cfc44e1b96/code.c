289778f42a77a5cca2a739f7c5ee8c9eabfa7a5c
Yamini Baluni <yaminibaluni607@gmail.com>
2025-02-06 09:44:29 +0530

    Solution to Grade Based on Marks

---
+++ Grade Based on Marks@@6788dd63490637cfc44e1b96/code.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    if (n>=80 && n<=90){
+        printf("A\n");
+    }
+    else if(n>=70 && n<=80){
+        printf("C\n");
+    }
+    else if(n>=60 && n<=70){
+        printf("D\n");
+    }
+return 0;
+}
